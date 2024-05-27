0d33acdae15ea6dae58f31977f8f39d8b1494cf7
Heitor Ferreira <heitorasf24@outlook.com>
2024-05-27 18:59:43 -0300

    Ajustes feitos para desafio celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Instanciar um Nokia
+Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111111111111", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+// Instanciar um iPhone
+Smartphone iphone = new Iphone("789012", "iPhone 13", "222222222222222", 128);
+iphone.InstalarAplicativo("Instagram");
+iphone.Ligar();
+iphone.ReceberLigacao();
